54a637ed8b39419ce3bc4a48ec9f7dda5fad2083
SlimmeYummy <zzzcccnnn@outlook.com>
2024-11-11 23:43:18 +0800

    utils shape

--- critical-point/core/src/utils/mod.rs
+++ critical-point/core/src/utils/mod.rs
@@ -5,6 +5,7 @@ mod id;
 mod key;
 mod math;
 mod ptr;
+mod shape;
 mod symbol;
 
 pub use collection::*;
@@ -13,11 +14,9 @@ pub use id::*;
 pub use key::*;
 pub use math::*;
 pub use ptr::*;
+pub use shape::*;
 pub use symbol::*;
 
 pub(crate) use extend::{extend, interface};
 
 pub type Num = f64;
-
-pub const FPS: u32 = 15;
-pub const MAX_PLAYER: usize = 8;
