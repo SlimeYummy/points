31ba73942fbccfe600100dded62e1c5e1ef67273
SlimmeYummy <zzzcccnnn@outlook.com>
2024-11-21 22:21:32 +0800

    logic action base

--- critical-point/core/src/logic/action/mod.rs
+++ critical-point/core/src/logic/action/mod.rs
@@ -4,24 +4,29 @@ mod r#move;
 
 pub use base::*;
 pub use idle::*;
+pub use r#move::*;
 
 use std::rc::Rc;
 
 use crate::instance::InstAction;
 use crate::logic::game::ContextUpdate;
-use crate::template::TmplClass;
+use crate::template::TmplType;
 use crate::utils::{CastPtr, CastRef, XError, XResult};
 
 pub(crate) fn new_logic_action(
     ctx: &mut ContextUpdate<'_>,
     inst_act: Rc<dyn InstAction>,
 ) -> XResult<Box<dyn LogicAction>> {
-    use TmplClass::*;
+    use TmplType::*;
 
-    let logic_act: Box<dyn LogicAction> = match inst_act.class() {
+    let logic_act: Box<dyn LogicAction> = match inst_act.typ() {
         ActionIdle => {
             let inst_act = unsafe { inst_act.cast_as_unchecked() };
             Box::new(LogicActionIdle::new(ctx, inst_act)?)
+        }
+        ActionMove => {
+            let inst_act = unsafe { inst_act.cast_as_unchecked() };
+            Box::new(LogicActionMove::new(ctx, inst_act)?)
         }
         _ => return Err(XError::BadType),
     };
@@ -33,13 +38,20 @@ pub(crate) fn try_reuse_logic_action(
     ctx: &mut ContextUpdate<'_>,
     inst_act: Rc<dyn InstAction>,
 ) -> XResult<bool> {
-    use TmplClass::*;
+    use TmplType::*;
 
-    match inst_act.class() {
+    match inst_act.typ() {
         ActionIdle => {
             if let Ok(logic_act) = logic_act.cast_mut::<LogicActionIdle>() {
                 let inst_act = unsafe { inst_act.cast_as_unchecked() };
                 *logic_act = LogicActionIdle::new(ctx, inst_act)?;
+                return Ok(true);
+            }
+        }
+        ActionMove => {
+            if let Ok(logic_act) = logic_act.cast_mut::<LogicActionMove>() {
+                let inst_act = unsafe { inst_act.cast_as_unchecked() };
+                *logic_act = LogicActionMove::new(ctx, inst_act)?;
                 return Ok(true);
             }
         }
