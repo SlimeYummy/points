c5113f91242aecddf85b47a99e96eea06ca9f32b
SlimmeYummy <zzzcccnnn@outlook.com>
2024-11-24 19:50:50 +0800

    logic system state

---
+++ critical-point/core/src/logic/system/state.rs
@@ -0,0 +1,238 @@
+use cirtical_point_csgen::CsOut;
+use std::collections::{vec_deque, VecDeque};
+use std::ops::{Index, RangeBounds};
+use std::sync::Arc;
+
+use super::super::base::StateAny;
+use crate::consts::FPS;
+use crate::utils::{XError, XResult};
+
+#[repr(C)]
+#[derive(Debug, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, CsOut)]
+#[cs_attr(Ref)]
+pub struct StateSet {
+    pub frame: u32,
+    pub inits: Vec<Arc<dyn StateAny>>,
+    pub updates: Vec<Box<dyn StateAny>>,
+}
+
+#[cfg(debug_assertions)]
+impl Drop for StateSet {
+    fn drop(&mut self) {
+        println!("StateSet drop() frame:{}", self.frame);
+    }
+}
+
+impl StateSet {
+    #[inline]
+    pub fn new(frame: u32, new_cap: usize, update_cap: usize) -> StateSet {
+        StateSet {
+            frame,
+            inits: Vec::with_capacity(new_cap),
+            updates: Vec::with_capacity(update_cap),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct SystemState {
+    state_sets: VecDeque<Arc<StateSet>>,
+    current_frame: u32,
+    synced_frame: u32,
+}
+
+impl Default for SystemState {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SystemState {
+    pub fn new() -> SystemState {
+        SystemState {
+            state_sets: VecDeque::with_capacity(2 * FPS as usize),
+            current_frame: 0,
+            synced_frame: 0,
+        }
+    }
+
+    #[inline]
+    pub fn current_frame(&self) -> u32 {
+        self.current_frame
+    }
+
+    #[inline]
+    pub fn synced_frame(&self) -> u32 {
+        self.synced_frame
+    }
+
+    #[inline]
+    pub fn unsynced_frame(&self) -> u32 {
+        self.synced_frame + 1
+    }
+
+    pub fn init(&mut self, state_set: Arc<StateSet>) -> XResult<()> {
+        if state_set.frame != 0 {
+            return Err(XError::bad_argument("SystemState::init() state_set.frame"));
+        }
+        self.state_sets.push_back(state_set);
+        Ok(())
+    }
+
+    pub fn append(&mut self, state_set: Arc<StateSet>) -> XResult<()> {
+        if state_set.frame != self.current_frame + 1 {
+            return Err(XError::bad_argument("SystemState::append() state_set.frame"));
+        }
+        self.current_frame += 1;
+        self.state_sets.push_back(state_set);
+        Ok(())
+    }
+
+    pub fn confirm(&mut self, synced_frame: u32) -> XResult<Vec<Arc<StateSet>>> {
+        let mut outs = vec![];
+        if synced_frame > self.current_frame {
+            return Err(XError::bad_argument("SystemState::confirm() synced_frame"));
+        }
+        if synced_frame <= self.synced_frame {
+            return Ok(outs);
+        }
+
+        while let Some(state) = self.state_sets.front() {
+            if state.frame < synced_frame {
+                let state_set = self.state_sets.pop_front();
+                assert!(state_set.is_some());
+                if let Some(state_set) = state_set {
+                    outs.push(state_set);
+                }
+            } else {
+                break;
+            }
+        }
+        self.synced_frame = synced_frame;
+        Ok(outs)
+    }
+
+    pub fn restore(&mut self, frame: u32) -> XResult<()> {
+        if frame < self.synced_frame || frame > self.current_frame {
+            return Err(XError::bad_argument("SystemState::restore() frame"));
+        }
+        while let Some(state) = self.state_sets.back() {
+            if state.frame > frame {
+                self.state_sets.pop_front();
+            } else {
+                break;
+            }
+        }
+        self.current_frame = frame;
+        Ok(())
+    }
+
+    #[inline]
+    pub fn get(&self, frame: u32) -> Option<&Arc<StateSet>> {
+        if frame < self.synced_frame || frame > self.current_frame {
+            return None;
+        }
+        return self.state_sets.get((frame - self.synced_frame) as usize);
+    }
+
+    pub fn range<R>(&self, frame_range: R) -> XResult<vec_deque::Iter<'_, Arc<StateSet>>>
+    where
+        R: RangeBounds<u32>,
+    {
+        let start = match frame_range.start_bound() {
+            std::ops::Bound::Included(&start) => start,
+            std::ops::Bound::Excluded(&start) => start + 1,
+            std::ops::Bound::Unbounded => self.synced_frame,
+        };
+        if start < self.synced_frame {
+            return Err(XError::bad_argument("SystemState::range() start"));
+        }
+        let start_pos = (start - self.synced_frame) as usize;
+
+        let end = match frame_range.end_bound() {
+            std::ops::Bound::Included(&end) => end + 1,
+            std::ops::Bound::Excluded(&end) => end,
+            std::ops::Bound::Unbounded => self.current_frame + 1,
+        };
+        let end_pos = (end - self.synced_frame) as usize;
+        if end_pos > self.state_sets.len() {
+            return Err(XError::bad_argument("SystemState::range() end"));
+        }
+
+        return Ok(self.state_sets.range(start_pos..end_pos));
+    }
+}
+
+impl Index<u32> for SystemState {
+    type Output = Arc<StateSet>;
+
+    fn index(&self, frame: u32) -> &Arc<StateSet> {
+        return self.get(frame).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_state_manager() {
+        let mut ss = SystemState::new();
+        assert_eq!(ss.current_frame(), 0);
+        assert_eq!(ss.synced_frame(), 0);
+        assert_eq!(ss.unsynced_frame(), 1);
+
+        assert!(ss.init(Arc::new(StateSet::new(1, 0, 0))).is_err());
+        ss.init(Arc::new(StateSet::new(0, 0, 0))).unwrap();
+        assert_eq!(ss.current_frame(), 0);
+        assert_eq!(ss.synced_frame(), 0);
+        assert_eq!(ss.unsynced_frame(), 1);
+        assert_eq!(ss.state_sets.len(), 1);
+        assert_eq!(ss.range(0..0).unwrap().count(), 0);
+        assert_eq!(ss.range(0..1).unwrap().count(), 1);
+
+        assert!(ss.confirm(2).is_err());
+        assert!(ss.restore(2).is_err());
+
+        // frame=1
+        ss.append(Arc::new(StateSet::new(1, 0, 0))).unwrap();
+        assert_eq!(ss.current_frame(), 1);
+        assert_eq!(ss.synced_frame(), 0);
+        assert_eq!(ss.unsynced_frame(), 1);
+        assert_eq!(ss.state_sets.len(), 2);
+        assert_eq!(ss.range(0..=1).unwrap().count(), 2);
+
+        ss.confirm(1).unwrap();
+        assert_eq!(ss.current_frame(), 1);
+        assert_eq!(ss.synced_frame(), 1);
+        assert_eq!(ss.unsynced_frame(), 2);
+        assert_eq!(ss.state_sets.len(), 1);
+        assert_eq!(ss.range(1..=1).unwrap().count(), 1);
+
+        ss.restore(1).unwrap();
+        assert_eq!(ss.current_frame(), 1);
+        assert_eq!(ss.synced_frame(), 1);
+        assert_eq!(ss.unsynced_frame(), 2);
+        assert_eq!(ss.state_sets.len(), 1);
+        assert_eq!(ss.range(1..=1).unwrap().count(), 1);
+
+        //frame=4
+        ss.append(Arc::new(StateSet::new(2, 0, 0))).unwrap();
+        ss.append(Arc::new(StateSet::new(3, 0, 0))).unwrap();
+        ss.append(Arc::new(StateSet::new(4, 0, 0))).unwrap();
+        assert_eq!(ss.current_frame(), 4);
+        assert_eq!(ss.synced_frame(), 1);
+        assert_eq!(ss.unsynced_frame(), 2);
+        assert_eq!(ss.state_sets.len(), 4);
+        assert_eq!(ss.range(1..=4).unwrap().count(), 4);
+
+        ss.confirm(3).unwrap();
+        assert_eq!(ss.current_frame(), 4);
+        assert_eq!(ss.synced_frame(), 3);
+        assert_eq!(ss.unsynced_frame(), 4);
+        assert_eq!(ss.state_sets.len(), 2);
+        assert_eq!(ss.range(3..=4).unwrap().count(), 2);
+
+        assert!(ss.confirm(5).is_err());
+    }
+}
