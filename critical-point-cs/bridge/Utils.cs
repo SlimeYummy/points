8399e853faeb8fc93c7254880241c6811322a1b7
SlimmeYummy <zzzcccnnn@outlook.com>
2025-02-04 23:06:02 +0800

    cs utils

---
+++ critical-point-cs/bridge/Utils.cs
@@ -0,0 +1,28 @@
+﻿using MessagePack;
+using MessagePack.Formatters;
+using MessagePack.Resolvers;
+
+namespace CriticalPoint {
+
+    internal class Static {
+        private static readonly Static _instance = new Static();
+
+        private MessagePackSerializerOptions _msgPackOpts;
+
+        private Static() {
+            var resolver = CompositeResolver.Create(
+                new IMessagePackFormatter[] {
+                    new Vec2Formatter(),
+                    new Vec3Formatter(),
+                    new Vec4Formatter(),
+                    new QuatFormatter(),
+                    new Mat4Formatter(),
+                },
+                new IFormatterResolver[] { StandardResolver.Instance }
+            );
+            _msgPackOpts = MessagePackSerializerOptions.Standard.WithResolver(resolver);
+        }
+
+        public static MessagePackSerializerOptions MsgPackOpts => _instance._msgPackOpts;
+    }
+}
