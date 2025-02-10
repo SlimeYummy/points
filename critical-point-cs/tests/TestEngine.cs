bc2f56ecf6b66d0ccd643b6f4a7ce0ee234b9745
SlimmeYummy <zzzcccnnn@outlook.com>
2025-02-10 20:17:07 +0800

    cs test engine

---
+++ critical-point-cs/tests/TestEngine.cs
@@ -0,0 +1,92 @@
+﻿using CriticalPoint;
+
+namespace CriticalPointTests {
+    [TestClass]
+    public class TestEngine {
+        const string TMPL_PATH = "../../../../../critical-point/test-res";
+        const string ASSET_PATH = "../../../../../critical-point/test-asset";
+
+        [TestMethod]
+        public void TestNewDelete() {
+            Assert.ThrowsException<EngineException>(() => {
+                LogicEngine engine = new LogicEngine("./test-res", "./test-asset");
+            });
+
+            LogicEngine engine = new LogicEngine(TMPL_PATH, ASSET_PATH);
+            Assert.IsNotNull(engine);
+            engine.Dispose();
+        }
+
+        [TestMethod]
+        public void TestVerifyPlayer() {
+            using (var engine = new LogicEngine(TMPL_PATH, ASSET_PATH)) {
+                var player1 = NewParamPlayer();
+                Assert.AreEqual("OK", engine.VerifyPlayer(player1));
+
+                var player2 = NewParamPlayer();
+                player2.level = 10;
+                Assert.AreNotEqual("OK", engine.VerifyPlayer(player2));
+
+                var player3 = NewParamPlayer();
+                player3.equipments = new List<IDLevel> {
+                    new IDLevel { id = "Equipment.No1", level = 1 },
+                    new IDLevel { id = "Equipment.No3", level = 0 },
+                };
+                Assert.AreNotEqual("OK", engine.VerifyPlayer(player3));
+            }
+        }
+
+        ParamPlayer NewParamPlayer() {
+            return new ParamPlayer {
+                character = "Character.No1",
+                style = "Style.No1-1",
+                level = 6,
+                equipments = new List<IDLevel> {
+                    new IDLevel { id = "Equipment.No1", level = 4 },
+                    new IDLevel { id = "Equipment.No2", level = 3 },
+                },
+                perks = new List<string> {
+                    "Perk.No1.AttackUp",
+                    "Perk.No1.Slot",
+                },
+                accessories = new List<ParamAccessory> {
+                    new ParamAccessory { id = "Accessory.AttackUp.Variant1", level = 0, entries = new List<string> {"Entry.DefenseUp"} },
+                },
+                jewels = new List<IDPlus> {
+                    new IDPlus { id = "Jewel.AttackUp.Variant1", plus = 0 },
+                },
+            };
+        }
+
+        ParamStage NewParamStage() {
+            return new ParamStage {
+                stage = "Stage.Demo",
+            };
+        }
+
+        [TestMethod]
+        public void TestStartGame() {
+            using (var engine = new LogicEngine(TMPL_PATH, ASSET_PATH)) {
+                var stage = NewParamStage();
+                var player = NewParamPlayer();
+
+                var state_set = engine.StartGame(stage, new List<ParamPlayer> { player });
+                Assert.AreEqual(state_set.inits.Length, 3);
+                state_set.Dispose();
+
+                var state_sets = engine.UpdateGame(new List<PlayerKeyEvents> {
+                    new PlayerKeyEvents {
+                        frame = 1,
+                        player_id = 3,
+                        events = new List<KeyEvent> {
+                             new KeyEvent { key = KeyCode.Attack1, pressed = true },
+                        },
+                    }
+                });
+                state_sets.Dispose();
+
+                engine.StopGame();
+            }
+        }
+    }
+}
