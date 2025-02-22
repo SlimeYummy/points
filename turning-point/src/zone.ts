6fd69fc4d7845c6ff70a68f2578844a2a06ca5fc
SlimmeYummy <zzzcccnnn@outlook.com>
2025-02-22 22:44:22 +0800

    config zone

---
+++ turning-point/src/zone.ts
@@ -0,0 +1,46 @@
+import { FilePath, ID, IDPrefix, MAX_NAME_LEN, parseFile, parseString } from './common';
+import { Resource } from './resource';
+
+export type ZoneArgs = {
+    /** Zone名字 */
+    name: string;
+
+    /** Zone文件路径（逻辑） */
+    zone_file: FilePath;
+
+    /** Zone文件路径（渲染） */
+    view_zone_file: FilePath;
+};
+
+/**
+ * 区域 游戏中的场景
+ */
+export class Zone extends Resource {
+    public static override prefix: IDPrefix = 'Zone';
+
+    public static override find(id: string, where: string): Zone {
+        const res = Resource.find(id, where);
+        if (!(res instanceof Zone)) {
+            throw new Error(`${where}: Resource type miss match`);
+        }
+        return res;
+    }
+
+    /** Zone名字 */
+    public readonly name: string;
+
+    /** Zone文件路径（逻辑） */
+    public readonly zone_file: FilePath;
+
+    /** Zone文件路径（渲染） */
+    public readonly view_zone_file: FilePath;
+
+    public constructor(id: ID, args: ZoneArgs) {
+        super(id);
+        this.name = parseString(args.name, this.w('name'), { max_len: MAX_NAME_LEN });
+        this.zone_file = parseFile(args.zone_file, this.w('zone_file'), { extension: '.json' });
+        this.view_zone_file = parseFile(args.view_zone_file, this.w('view_zone_file'));
+    }
+
+    public override verify() {}
+}
