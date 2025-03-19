a7580fe1e517486dac7ddb57d08c2c2f3c086b51
SlimmeYummy <zzzcccnnn@outlook.com>
2025-03-19 23:07:56 +0800

    asset stage

--- critical-point/core/src/asset/stage.rs
+++ critical-point/core/src/asset/stage.rs
@@ -1,41 +1,55 @@
 use glam::{Quat, Vec3A};
-use jolt_physics_rs::{self as jolt, BodyID, BodySettings, PHY_LAYER_STATIC};
-use serde::Deserialize;
+use jolt_physics_rs::{BodyCreationSettings, BodyID, BodyInterface};
 
 use crate::asset::loader::AssetLoader;
-use crate::asset::shape::AssetShapeEx;
-use crate::utils::{NumID, XResult};
+use crate::asset::shape::{default_position, default_rotation, AssetShape};
+use crate::logic::PHY_LAYER_STATIC;
+use crate::utils::{xerrf, xfrom, XResult};
 
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, serde::Serialize, serde::Deserialize)]
 pub struct AssetStage {
-    shapes: Vec<AssetShapeEx>,
+    shapes: Vec<AssetShape>,
     bodies: Vec<AssetStageBody>,
 }
 
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, serde::Serialize, serde::Deserialize)]
 pub struct AssetStageBody {
-    shape_id: NumID,
-    #[serde(default = "crate::utils::default_position")]
+    shape_index: u32,
+    #[serde(default = "default_position")]
     position: Vec3A,
-    #[serde(default = "crate::utils::default_rotation")]
+    #[serde(default = "default_rotation")]
     rotation: Quat,
 }
 
+#[derive(Debug, Default)]
+pub struct LoadedStage {
+    pub bodies: Vec<BodyID>,
+}
+
 impl AssetLoader {
-    pub fn load_stage(&mut self, file: &str) -> XResult<Vec<BodyID>> {
-        let stage = self.load_json::<AssetStage>(file)?;
-        for shape in &stage.shapes {
-            self.load_shape_ex(shape)?;
+    pub fn load_stage(&mut self, file: &str, body_itf: &mut BodyInterface) -> XResult<LoadedStage> {
+        let asset_stage = self.load_json::<AssetStage, _>(file)?;
+
+        let mut jolt_shapes = Vec::with_capacity(asset_stage.shapes.len());
+        for shape in &asset_stage.shapes {
+            jolt_shapes.push(self.load_shape(shape)?);
         }
 
-        let mut bodies = Vec::with_capacity(stage.bodies.len());
-        for object in &stage.bodies {
-            let ref_shape = self.get_shape_ex(object.shape_id)?;
-            let settings = BodySettings::new_static(ref_shape, PHY_LAYER_STATIC, object.position, object.rotation);
-            let body = self.create_body(&settings)?;
+        let mut bodies = Vec::with_capacity(asset_stage.bodies.len());
+        for asset_body in &asset_stage.bodies {
+            let jolt_shape = jolt_shapes
+                .get(asset_body.shape_index as usize)
+                .ok_or_else(|| xerrf!(BadAsset; "file={} shape_index={}", file, asset_body.shape_index))?;
+            let settings = BodyCreationSettings::new_static(
+                jolt_shape.clone(),
+                PHY_LAYER_STATIC,
+                asset_body.position,
+                asset_body.rotation,
+            );
+            let body = body_itf.create_body(&settings).map_err(xfrom!())?;
             bodies.push(body);
         }
 
-        Ok(bodies)
+        Ok(LoadedStage { bodies })
     }
 }
