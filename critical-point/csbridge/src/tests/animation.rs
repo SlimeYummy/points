1105680626084928ef1c66ddc9ee77c7cb596165
SlimmeYummy <zzzcccnnn@outlook.com>
2025-04-20 19:14:50 +0800

    update CS bridge

--- critical-point/csbridge/src/tests/animation.rs
+++ critical-point/csbridge/src/tests/animation.rs
@@ -2,7 +2,7 @@
 
 use cirtical_point_core::logic::{ActionIdleMode, StateAction, StateActionBase, StateActionIdle, StateActionType};
 use cirtical_point_core::template::TmplType;
-use cirtical_point_core::utils::s;
+use cirtical_point_core::utils::asb;
 
 #[no_mangle]
 pub extern "C" fn mock_skeleton_animator_state_actions() -> Vec<Box<dyn StateAction>> {
@@ -15,8 +15,8 @@ pub extern "C" fn mock_skeleton_animator_state_actions() -> Vec<Box<dyn StateAct
         switch_progress: 0,
     });
     idle._base.id = 111;
-    idle._base.tmpl_id = s!("idle");
-    idle.animations[0].file = s!("girl_animation_logic_stand_idle.ozz");
+    idle._base.tmpl_id = asb!("idle");
+    idle.animations[0].file = asb!("view_anim.ozz");
     idle.animations[0].animation_id = 1;
     idle.animations[0].ratio = 0.0;
     idle.animations[0].weight = 1.0;
