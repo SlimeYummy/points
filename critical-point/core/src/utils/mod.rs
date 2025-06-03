d0138f7a47f0d765d28d4e548f54fb9a6e12e81b
SlimmeYummy <zzzcccnnn@outlook.com>
2025-06-03 22:15:24 +0800

    utils keys

--- critical-point/core/src/utils/mod.rs
+++ critical-point/core/src/utils/mod.rs
@@ -4,20 +4,21 @@ mod id;
 mod key;
 mod macros;
 mod math;
+mod miscs;
 mod ptr;
 mod shape;
 mod symbol;
+
+#[cfg(test)]
+pub(crate) mod tests;
 
 pub use collection::*;
 pub use error::*;
 pub use id::*;
 pub use key::*;
+pub(crate) use macros::*;
 pub use math::*;
+pub use miscs::*;
 pub use ptr::*;
 pub use shape::*;
 pub use symbol::*;
-
-pub(crate) use macros::{extend, interface, rkyv_self, serde_by};
-pub(crate) use math::near;
-
-pub type Num = f64;
