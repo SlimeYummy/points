180d7a8e52fc2fbac893e5f3b51d48a314c1d6a6
SlimmeYummy <zzzcccnnn@outlook.com>
2025-06-10 19:23:46 +0800

    template accessory

--- critical-point/core/src/template/accessory.rs
+++ critical-point/core/src/template/accessory.rs
@@ -1,7 +1,6 @@
 use crate::consts::MAX_ENTRY_PLUS;
-use crate::template2::base::{ArchivedTmplAny, TmplAny, TmplRare, TmplType};
-use crate::template2::id::{TmplID, TmplHashMap};
-use crate::utils::rkyv_self;
+use crate::template::base::{impl_tmpl, TmplHashMap};
+use crate::utils::{impl_for, rkyv_self, RareLevel, TmplID};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
 pub enum TmplAccessoryPattern {
@@ -13,7 +12,7 @@ pub enum TmplAccessoryPattern {
 rkyv_self!(TmplAccessoryPattern);
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
-#[archive_attr(derive(Debug))]
+#[rkyv(derive(Debug))]
 pub struct TmplAccessoryPool {
     pub id: TmplID,
     pub patterns: Vec<TmplAccessoryPattern>,
@@ -22,28 +21,17 @@ pub struct TmplAccessoryPool {
     pub b_entries: TmplHashMap<u32>,
 }
 
-#[typetag::deserialize(name = "AccessoryPool")]
-impl TmplAny for TmplAccessoryPool {
-    #[inline]
-    fn id(&self) -> TmplID {
-        self.id.clone()
-    }
+impl_tmpl!(TmplAccessoryPool, AccessoryPool, "AccessoryPool");
 
+impl_for!(TmplAccessoryPool, ArchivedTmplAccessoryPool, {
     #[inline]
-    fn typ(&self) -> TmplType {
-        TmplType::AccessoryPool
-    }
-}
-
-impl TmplAccessoryPool {
-    #[inline]
-    pub fn main_plus(&self, level: u32, piece: u32) -> u32 {
+    pub fn calc_main_plus(&self, level: u32, main_piece: u32) -> u32 {
         let count = (self.patterns.len() + 1) as u32;
-        u32::min(level / count, MAX_ENTRY_PLUS) * piece
+        u32::min(level / count, MAX_ENTRY_PLUS) * main_piece
     }
 
     #[inline]
-    pub fn pool_plus(&self, level: u32, pos: u32) -> u32 {
+    pub fn calc_sub_plus(&self, level: u32, pos: u32) -> u32 {
         let count = (self.patterns.len() + 1) as u32;
         if level % count > pos {
             u32::min(level / count + 1, MAX_ENTRY_PLUS)
@@ -51,60 +39,25 @@ impl TmplAccessoryPool {
             u32::min(level / count, MAX_ENTRY_PLUS)
         }
     }
-}
-
-impl ArchivedTmplAny for ArchivedTmplAccessoryPool {
-    #[inline]
-    fn id(&self) -> TmplID {
-        self.id.clone()
-    }
-
-    #[inline]
-    fn typ(&self) -> TmplType {
-        TmplType::Accessory
-    }
-}
+});
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
-#[archive_attr(derive(Debug))]
+#[rkyv(derive(Debug))]
 pub struct TmplAccessory {
     pub id: TmplID,
     pub pool: TmplID,
-    pub rare: TmplRare,
+    pub rare: RareLevel,
     pub entry: TmplID,
     pub piece: u32,
 }
 
-#[typetag::deserialize(name = "Accessory")]
-impl TmplAny for TmplAccessory {
-    #[inline]
-    fn id(&self) -> TmplID {
-        self.id.clone()
-    }
-
-    #[inline]
-    fn typ(&self) -> TmplType {
-        TmplType::Accessory
-    }
-}
-
-impl ArchivedTmplAny for ArchivedTmplAccessory {
-    #[inline]
-    fn id(&self) -> TmplID {
-        self.id.clone()
-    }
-
-    #[inline]
-    fn typ(&self) -> TmplType {
-        TmplType::Accessory
-    }
-}
+impl_tmpl!(TmplAccessory, Accessory, "Accessory");
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::template2::database::TmplDatabase;
-    use crate::template2::id::id;
+    use crate::template::database::TmplDatabase;
+    use crate::utils::id;
 
     #[test]
     fn test_load_accessory_pool() {
@@ -144,13 +97,13 @@ mod tests {
 
         let a1 = db.find_as::<TmplAccessory>(id!("Accessory.CriticalChance")).unwrap();
         assert_eq!(a1.pool, id!("AccessoryPool.Rare2"));
-        assert_eq!(a1.rare, TmplRare::Rare2);
+        assert_eq!(a1.rare, RareLevel::Rare2);
         assert_eq!(a1.entry, id!("Entry.CriticalChance"));
         assert_eq!(a1.piece, 2);
 
         let a1 = db.find_as::<TmplAccessory>(id!("Accessory.AttackUp/3")).unwrap();
         assert_eq!(a1.pool, id!("AccessoryPool.Rare3"));
-        assert_eq!(a1.rare, TmplRare::Rare3);
+        assert_eq!(a1.rare, RareLevel::Rare3);
         assert_eq!(a1.entry, id!("Entry.AttackUp"));
         assert_eq!(a1.piece, 3);
     }
