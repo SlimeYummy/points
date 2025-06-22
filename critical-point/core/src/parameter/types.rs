3aba908a1d921e782a69d4683265c3ee06f00f4a
SlimmeYummy <zzzcccnnn@outlook.com>
2025-06-22 19:35:52 +0800

    parameter

--- critical-point/core/src/parameter/types.rs
+++ critical-point/core/src/parameter/types.rs
@@ -1,24 +1,24 @@
 use cirtical_point_csgen::CsIn;
 
-use crate::utils::{IDLevel2, IDPlus2, StrID};
+use crate::utils::{TmplID, TmplIDLevel, TmplIDPlus};
 
 #[derive(
     Debug, Default, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, serde::Serialize, serde::Deserialize, CsIn,
 )]
 #[cs_attr(Class)]
 pub struct ParamPlayer {
-    pub character: StrID,
-    pub style: StrID,
+    pub character: TmplID,
+    pub style: TmplID,
     #[serde(default)]
     pub level: u32,
     #[serde(default)]
-    pub equipments: Vec<IDLevel2>,
+    pub equipments: Vec<TmplIDLevel>,
     #[serde(default)]
-    pub perks: Vec<StrID>,
+    pub perks: Vec<TmplIDLevel>,
     #[serde(default)]
     pub accessories: Vec<ParamAccessory>,
     #[serde(default)]
-    pub jewels: Vec<IDPlus2>,
+    pub jewels: Vec<TmplIDPlus>,
 }
 
 #[derive(
@@ -26,15 +26,15 @@ pub struct ParamPlayer {
 )]
 #[cs_attr(Class)]
 pub struct ParamAccessory {
-    pub id: StrID,
+    pub id: TmplID,
     pub level: u32,
-    pub entries: Vec<StrID>,
+    pub entries: Vec<TmplID>,
 }
 
 #[derive(
     Debug, Default, Clone, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, serde::Serialize, serde::Deserialize, CsIn,
 )]
 #[cs_attr(Class)]
-pub struct ParamStage {
-    pub stage: StrID,
+pub struct ParamZone {
+    pub zone: TmplID,
 }
