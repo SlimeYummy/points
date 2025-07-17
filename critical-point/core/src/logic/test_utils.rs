da7b40bb072f83b1afa8c2b349bd4f26f4f4db35
SlimmeYummy <zzzcccnnn@outlook.com>
2025-07-17 19:45:09 +0800

    logic test utils

--- critical-point/core/src/logic/test_utils.rs
+++ critical-point/core/src/logic/test_utils.rs
@@ -1,12 +1,12 @@
+use jolt_physics_rs::PhysicsSystem;
 use std::fmt::Debug;
 use std::rc::Rc;
 
-use jolt_physics_rs::PhysicsSystem;
-
-use crate::consts::{TEST_ASSET_PATH, TEST_TEMPLATE_PATH};
+use crate::consts::TEST_ASSET_PATH;
 use crate::instance::{assemble_player, InstPlayer};
 use crate::logic::action::{
-    ArchivedStateAction, ContextAction, LogicAction, LogicActionBase, StateAction, StateActionBase, StateActionType,
+    impl_state_action, ContextAction, LogicActionAny, LogicActionBase, LogicActionStatus, StateActionAny,
+    StateActionBase, StateActionType,
 };
 use crate::logic::game::{ContextUpdate, LogicSystems};
 use crate::logic::physics::{
@@ -14,106 +14,50 @@ use crate::logic::physics::{
 };
 use crate::parameter::ParamPlayer;
 use crate::template::{TmplDatabase, TmplType};
-use crate::utils::{extend, sb, NumID, StrID, XResult};
+use crate::utils::{extend, id, NumID, TmplID, XResult, MIN_PLAYER_ID};
 
-pub(crate) fn mock_physics_system() -> PhysicsSystem {
-    PhysicsSystem::new(
-        BroadPhaseLayerInterfaceImpl::new_vbox(BroadPhaseLayerInterfaceImpl),
-        ObjectVsBroadPhaseLayerFilterImpl::new_vbox(ObjectVsBroadPhaseLayerFilterImpl),
-        ObjectLayerPairFilterImpl::new_vbox(ObjectLayerPairFilterImpl),
-    )
+pub(super) struct TestEnv {
+    pub systems: LogicSystems,
 }
 
-pub(crate) fn mock_logic_systems() -> LogicSystems {
-    let tmpl_db = TmplDatabase::new(TEST_TEMPLATE_PATH).unwrap();
-    LogicSystems::new(tmpl_db, TEST_ASSET_PATH, None).unwrap()
-}
+impl TestEnv {
+    pub const FRAME: u32 = 100;
+    pub const PLAYER_ID: NumID = MIN_PLAYER_ID + 1;
 
-pub(crate) fn mock_inst_player(systems: &mut LogicSystems) -> Rc<InstPlayer> {
-    let mut ctx = ContextUpdate::new(systems, 0, 0);
-    let param_player = ParamPlayer {
-        character: sb!("Character.No1"),
-        style: sb!("Style.No1-1"),
-        level: 4,
-        ..Default::default()
-    };
-    let inst_player = assemble_player(&mut ctx.context_assemble(), &param_player).unwrap();
-    Rc::new(inst_player)
-}
+    pub fn new() -> XResult<TestEnv> {
+        let db = TmplDatabase::new(10240, 150)?;
+        let systems = LogicSystems::new(db, TEST_ASSET_PATH, None)?;
+        Ok(TestEnv { systems })
+    }
 
-#[derive(Debug, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
-#[archive_attr(derive(Debug))]
-pub(crate) struct StateActionEmpty {
-    pub _base: StateActionBase,
-}
-
-extend!(StateActionEmpty, StateActionBase);
-
-impl Default for StateActionEmpty {
-    fn default() -> Self {
-        StateActionEmpty {
-            _base: StateActionBase::new(StateActionType::Idle, TmplType::ActionIdle),
-        }
+    pub fn context_update(&mut self) -> ContextUpdate<'_> {
+        ContextUpdate::new(&mut self.systems, Self::FRAME, 95)
     }
 }
 
-unsafe impl StateAction for StateActionEmpty {
-    fn typ(&self) -> StateActionType {
-        StateActionType::Idle
-    }
+// pub(crate) fn mock_physics_system() -> PhysicsSystem {
+//     PhysicsSystem::new(
+//         BroadPhaseLayerInterfaceImpl::new_vbox(BroadPhaseLayerInterfaceImpl),
+//         ObjectVsBroadPhaseLayerFilterImpl::new_vbox(ObjectVsBroadPhaseLayerFilterImpl),
+//         ObjectLayerPairFilterImpl::new_vbox(ObjectLayerPairFilterImpl),
+//     )
+// }
 
-    fn tmpl_typ(&self) -> TmplType {
-        TmplType::ActionIdle
-    }
-}
+// pub(crate) fn mock_logic_systems() -> LogicSystems {
+//     let db = TmplDatabase::new(10240, 150).unwrap();
+//     LogicSystems::new(db, TEST_ASSET_PATH, None).unwrap()
+// }
 
-#[allow(private_interfaces)]
-impl ArchivedStateAction for rkyv::Archived<StateActionEmpty> {
-    fn typ(&self) -> StateActionType {
-        StateActionType::Idle
-    }
+// pub(crate) fn mock_logic_chara_physics(player_id: NumID, inst_player: Rc<InstPlayer>) {}
 
-    fn tmpl_typ(&self) -> TmplType {
-        TmplType::ActionIdle
-    }
-}
-
-#[repr(C)]
-#[derive(Debug)]
-pub(crate) struct LogicActionEmpty {
-    _base: LogicActionBase,
-}
-
-extend!(LogicActionEmpty, LogicActionBase);
-
-impl LogicActionEmpty {
-    pub(crate) fn new(id: NumID) -> Box<LogicActionEmpty> {
-        Box::new(LogicActionEmpty {
-            _base: LogicActionBase::new(id, StrID::default(), 1),
-        })
-    }
-}
-
-unsafe impl LogicAction for LogicActionEmpty {
-    fn typ(&self) -> StateActionType {
-        StateActionType::Idle
-    }
-
-    fn tmpl_typ(&self) -> TmplType {
-        TmplType::ActionIdle
-    }
-
-    fn restore(&mut self, _state: &(dyn StateAction + 'static)) -> XResult<()> {
-        Ok(())
-    }
-
-    fn update(
-        &mut self,
-        _ctx: &mut ContextUpdate<'_>,
-        _ctx_au: &mut ContextAction<'_>,
-    ) -> XResult<Option<Box<dyn StateAction>>> {
-        Ok(Some(Box::new(StateActionEmpty {
-            _base: StateActionBase::new(StateActionType::Idle, TmplType::ActionIdle),
-        })))
-    }
-}
+// pub(crate) fn mock_inst_player(systems: &mut LogicSystems) -> Rc<InstPlayer> {
+//     let mut ctx = ContextUpdate::new(systems, 0, 0);
+//     let param_player = ParamPlayer {
+//         character: id!("Character.One"),
+//         style: id!("Style.One/1"),
+//         level: 4,
+//         ..Default::default()
+//     };
+//     let inst_player = assemble_player(&mut ctx.context_assemble(), &param_player).unwrap();
+//     Rc::new(inst_player)
+// }
