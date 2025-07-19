ca2be787ec304ef3923273dbde9d6dcb50209e8b
SlimmeYummy <zzzcccnnn@outlook.com>
2025-07-19 21:00:13 +0800

    logic character

--- critical-point/core/src/logic/character/player.rs
+++ critical-point/core/src/logic/character/player.rs
@@ -1,104 +1,51 @@
 use cirtical_point_csgen::CsOut;
-use glam::{Quat, Vec3A};
+use glam::Vec3A;
 use std::rc::Rc;
 use std::sync::Arc;
 
 use super::action::LogicCharaAction;
 use super::physics::{LogicCharaPhysics, StateCharaPhysics};
+use crate::consts::DEFAULT_TOWARD_DIR_2D;
 use crate::instance::{assemble_player, InstPlayer};
-use crate::logic::action::StateAction;
-use crate::logic::base::{ArchivedStateAny, LogicAny, LogicType, StateAny, StateAnyBase, StateType};
+use crate::logic::action::StateActionAny;
+use crate::logic::base::{impl_state, LogicAny, LogicType, StateBase, StateType};
 use crate::logic::game::{ContextRestore, ContextUpdate};
 use crate::parameter::ParamPlayer;
-use crate::template::{TmplCharacter, TmplStyle};
-use crate::utils::{extend, ASymbol, NumID, XResult};
+use crate::template::TmplStyle;
+use crate::utils::{extend, sb, NumID, Symbol, XResult};
 
 #[repr(C)]
-#[derive(Debug, PartialEq, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, CsOut)]
-#[archive_attr(derive(Debug))]
+#[derive(
+    Debug, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, CsOut,
+)]
+#[rkyv(derive(Debug))]
 #[cs_attr(Ref)]
 pub struct StatePlayerInit {
-    pub _base: StateAnyBase,
-    pub skeleton_file: ASymbol,
-    pub animation_files: Vec<ASymbol>,
-    pub view_model: String,
+    pub _base: StateBase,
+    pub skeleton_file: Symbol,
+    pub animation_files: Vec<Symbol>,
+    pub view_model: Symbol,
 }
 
-extend!(StatePlayerInit, StateAnyBase);
+extend!(StatePlayerInit, StateBase);
 
-unsafe impl StateAny for StatePlayerInit {
-    #[inline]
-    fn typ(&self) -> StateType {
-        assert_eq!(self.typ, StateType::PlayerInit);
-        StateType::PlayerInit
-    }
-
-    #[inline]
-    fn id(&self) -> NumID {
-        self.id
-    }
-
-    #[inline]
-    fn logic_typ(&self) -> LogicType {
-        assert_eq!(self.logic_typ, LogicType::Player);
-        LogicType::Player
-    }
-}
-
-impl ArchivedStateAny for rkyv::Archived<StatePlayerInit> {
-    #[inline]
-    fn typ(&self) -> StateType {
-        StateType::PlayerInit
-    }
-
-    #[inline]
-    fn logic_typ(&self) -> LogicType {
-        LogicType::Player
-    }
-}
+impl_state!(StatePlayerInit, Player, PlayerInit, "PlayerInit");
 
 #[repr(C)]
-#[derive(Debug, PartialEq, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, CsOut)]
-#[archive_attr(derive(Debug))]
+#[derive(
+    Debug, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, CsOut,
+)]
+#[rkyv(derive(Debug))]
 #[cs_attr(Ref)]
 pub struct StatePlayerUpdate {
-    pub _base: StateAnyBase,
+    pub _base: StateBase,
     pub physics: StateCharaPhysics,
-    pub actions: Vec<Box<dyn StateAction>>,
+    pub actions: Vec<Box<dyn StateActionAny>>,
 }
 
-extend!(StatePlayerUpdate, StateAnyBase);
+extend!(StatePlayerUpdate, StateBase);
 
-unsafe impl StateAny for StatePlayerUpdate {
-    #[inline]
-    fn typ(&self) -> StateType {
-        assert!(self.typ == StateType::PlayerUpdate);
-        StateType::PlayerUpdate
-    }
-
-    #[inline]
-    fn id(&self) -> NumID {
-        self.id
-    }
-
-    #[inline]
-    fn logic_typ(&self) -> LogicType {
-        assert!(self.logic_typ == LogicType::Player);
-        LogicType::Player
-    }
-}
-
-impl ArchivedStateAny for rkyv::Archived<StatePlayerUpdate> {
-    #[inline]
-    fn typ(&self) -> StateType {
-        StateType::PlayerUpdate
-    }
-
-    #[inline]
-    fn logic_typ(&self) -> LogicType {
-        LogicType::Player
-    }
-}
+impl_state!(StatePlayerUpdate, Player, PlayerUpdate, "PlayerUpdate");
 
 #[derive(Debug)]
 pub struct LogicPlayer {
@@ -137,8 +84,7 @@ impl LogicPlayer {
         ctx: &mut ContextUpdate<'_>,
         param_player: &ParamPlayer,
     ) -> XResult<(Box<LogicPlayer>, Arc<StatePlayerInit>)> {
-        let tmpl_chara = ctx.tmpl_db.find_as::<TmplCharacter>(&param_player.character)?;
-        let tmpl_style = ctx.tmpl_db.find_as::<TmplStyle>(&param_player.style)?;
+        let tmpl_style = ctx.tmpl_db.find_as::<TmplStyle>(param_player.style)?;
 
         let inst_player = Rc::new(assemble_player(&mut ctx.context_assemble(), param_player)?);
         let player_id = ctx.gene.gen_player_id()?;
@@ -147,16 +93,22 @@ impl LogicPlayer {
             spawn_frame: ctx.frame,
             death_frame: u32::MAX,
             inst: inst_player.clone(),
-            chara_physics: LogicCharaPhysics::new(ctx, player_id, inst_player.clone(), Vec3A::ZERO, Quat::IDENTITY)?,
+            chara_physics: LogicCharaPhysics::new(
+                ctx,
+                player_id,
+                inst_player.clone(),
+                Vec3A::ZERO,
+                DEFAULT_TOWARD_DIR_2D,
+            )?,
             chara_action: LogicCharaAction::new(ctx, player_id, inst_player.clone())?,
         });
 
         let animation_files = player.chara_action.preload_assets(ctx, inst_player.clone())?;
         let state_init = Arc::new(StatePlayerInit {
-            _base: StateAnyBase::new(player.id, StateType::PlayerInit, LogicType::Player),
-            skeleton_file: ASymbol::from(&tmpl_chara.skeleton),
+            _base: StateBase::new(player.id, StateType::PlayerInit, LogicType::Player),
+            skeleton_file: inst_player.skeleton_files.clone(),
             animation_files,
-            view_model: tmpl_style.view_model.clone(),
+            view_model: sb!(&tmpl_style.view_model),
         });
 
         player.chara_action.update(ctx, &player.chara_physics, true)?;
@@ -166,7 +118,7 @@ impl LogicPlayer {
 
     pub fn state(&mut self) -> XResult<Box<StatePlayerUpdate>> {
         Ok(Box::new(StatePlayerUpdate {
-            _base: StateAnyBase::new(self.id, StateType::PlayerUpdate, LogicType::Player),
+            _base: StateBase::new(self.id, StateType::PlayerUpdate, LogicType::Player),
             physics: self.chara_physics.state(),
             actions: self.chara_action.take_states()?,
         }))
@@ -190,35 +142,39 @@ impl LogicPlayer {
 mod tests {
     use super::*;
     use crate::logic::action::StateActionIdle;
-    use crate::logic::game::{ContextUpdate, LogicSystems};
     use crate::logic::test_utils::*;
-    use crate::utils::{asb, sb, CastRef};
+    use crate::utils::{id, Castable};
 
-    fn prepare(systems: &mut LogicSystems, frame: u32) -> (Box<LogicPlayer>, Arc<StatePlayerInit>, ContextUpdate<'_>) {
-        let mut ctx = ContextUpdate::new(systems, frame, 0);
+    fn prepare_player(tenv: &mut TestEnv) -> (Box<LogicPlayer>, Arc<StatePlayerInit>) {
         let param_player = ParamPlayer {
-            character: sb!("Character.No1"),
-            style: sb!("Style.No1-1"),
+            character: id!("Character.Instance/1"),
+            style: id!("Style.Instance/1A"),
             level: 4,
             ..Default::default()
         };
+        let mut ctx = tenv.context_update();
         let (logic_player, state_init) = LogicPlayer::new(&mut ctx, &param_player).unwrap();
         ctx.input.init(1).unwrap();
-        (logic_player, state_init, ctx)
+        (logic_player, state_init)
     }
 
     #[test]
-    fn test_logic_player_new() {
-        let mut systems = mock_logic_systems();
-        let (mut logic_player, state_init, _) = prepare(&mut systems, 0);
+    fn test_new() {
+        let mut tenv = TestEnv::new().unwrap();
+        let (mut logic_player, state_init) = prepare_player(&mut tenv);
         assert_eq!(logic_player.id, 100);
-        assert_eq!(logic_player.inst.tmpl_character, sb!("Character.No1"));
-        assert_eq!(logic_player.inst.tmpl_style, sb!("Style.No1-1"));
+        assert_eq!(logic_player.inst.tmpl_character, id!("Character.Instance/1"));
+        assert_eq!(logic_player.inst.tmpl_style, id!("Style.Instance/1A"));
 
         assert_eq!(state_init.id, 100);
-        assert_eq!(state_init.skeleton_file, sb!("skel.ozz"));
-        assert_eq!(state_init.animation_files.len(), 2);
-        let excepted_files = [asb!("anim_stand_idle.ozz"), asb!("anim_stand_ready.ozz")];
+        assert_eq!(state_init.skeleton_file, "girl");
+        assert_eq!(state_init.animation_files.len(), 4);
+        let excepted_files = [
+            sb!("girl_stand_idle"),
+            sb!("girl_stand_ready"),
+            sb!("girl_run"),
+            sb!("girl_attack1_1"),
+        ];
         for file in excepted_files.iter() {
             assert!(state_init.animation_files.contains(file));
         }
@@ -226,19 +182,19 @@ mod tests {
         let state_update = logic_player.state().unwrap();
         assert_eq!(state_update.id, 100);
         assert_eq!(state_update.actions.len(), 1);
-        let state_act = state_update.actions[0].cast_ref::<StateActionIdle>().unwrap();
-        assert_eq!(state_act.tmpl_id, sb!("Action.No1.Idle"));
+        let state_act = state_update.actions[0].as_ref().cast::<StateActionIdle>().unwrap();
+        assert_eq!(state_act.tmpl_id, id!("Action.Instance.Idle/1A"));
     }
 
     #[test]
     fn test_logic_player_update() {
-        let mut systems = mock_logic_systems();
-        let (mut logic_player, _, mut ctx) = prepare(&mut systems, 1);
-        logic_player.update(&mut ctx).unwrap();
+        let mut tenv = TestEnv::new().unwrap();
+        let (mut logic_player, _) = prepare_player(&mut tenv);
+        logic_player.update(&mut tenv.context_update()).unwrap();
         let state_update = logic_player.state().unwrap();
         assert_eq!(state_update.id, 100);
         assert_eq!(state_update.actions.len(), 1);
-        let state_act = state_update.actions[0].cast_ref::<StateActionIdle>().unwrap();
-        assert_eq!(state_act.tmpl_id, sb!("Action.No1.Idle"));
+        let state_act = state_update.actions[0].as_ref().cast::<StateActionIdle>().unwrap();
+        assert_eq!(state_act.tmpl_id, id!("Action.Instance.Idle/1A"));
     }
 }
