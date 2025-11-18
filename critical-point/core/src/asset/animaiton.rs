fcae5aba5044f25b2a187de1d1b87226a8219f0e
SlimmeYummy <zzzcccnnn@outlook.com>
2025-11-18 19:45:57 +0800

    assets

--- critical-point/core/src/asset/animaiton.rs
+++ critical-point/core/src/asset/animaiton.rs
@@ -1,7 +1,7 @@
 use ozz_animation_rs::{Animation, Archive, Skeleton};
 use std::rc::Rc;
 
-use crate::animation::RootMotionTrack;
+use crate::animation::{RootMotionTrack, WeaponMotionTrackSet};
 use crate::asset::loader::AssetLoader;
 use crate::utils::{xfromf, Symbol, XResult};
 
@@ -34,11 +34,23 @@ impl AssetLoader {
         if let Some(root_motion) = self.root_motion_cache.get(&path_pattern) {
             return Ok(root_motion.clone());
         }
-        let path = format!("{}.m-ozz", &path_pattern[0..path_pattern.len() - 2]);
+        let path = format!("{}.rm-ozz", &path_pattern[0..path_pattern.len() - 2]);
         let data_buf = self.load_buffer(&path)?;
         let mut archive = Archive::from_vec(data_buf).map_err(xfromf!("path={:?}", &path))?;
         let root_motion = Rc::new(RootMotionTrack::from_archive(&mut archive).map_err(xfromf!("path={:?}", &path))?);
         self.root_motion_cache.insert(path_pattern.clone(), root_motion.clone());
         Ok(root_motion)
+    }
+
+    pub fn load_weapon_motion(&mut self, path_pattern: Symbol) -> XResult<Rc<WeaponMotionTrackSet>> {
+        if let Some(weapon_track) = self.weapon_motion_cache.get(&path_pattern) {
+                return Ok(weapon_track.clone());
+        }
+        let path = format!("{}.wm-ozz", &path_pattern[0..path_pattern.len() - 2]);
+        let data_buf = self.load_buffer(&path)?;
+        let mut archive = Archive::from_vec(data_buf).map_err(xfromf!("path={:?}", &path))?;
+        let weapon_motion = Rc::new(WeaponMotionTrackSet::from_archive(&mut archive).map_err(xfromf!("path={:?}", &path))?);
+        self.weapon_motion_cache.insert(path_pattern.clone(), weapon_motion.clone());
+        Ok(weapon_motion)
     }
 }
