a12bc7b279f03d7758c0b9fc7bdeb5bf27409314
SlimmeYummy <zzzcccnnn@outlook.com>
2025-12-13 19:11:38 +0800

    test verify

--- turning-point/test/verify.ts
+++ turning-point/test/verify.ts
@@ -10,91 +10,91 @@ const fixed_attributes = {
     weak_damage_up: 0.25,
 };
 
-new Character('Character.Verify/1', {
+new Character('Character.Verify^1', {
     name: 'Character 1',
     level: [1, 3],
-    styles: ['Style.Verify/1A', 'Style.Verify/1B'],
-    equipments: ['Equipment.Verify/1A', 'Equipment.Verify/1B', 'Equipment.Verify/1C'],
+    styles: ['Style.Verify^1A', 'Style.Verify^1B'],
+    equipments: ['Equipment.Verify^1A', 'Equipment.Verify^1B', 'Equipment.Verify^1C'],
     bounding: new TaperedCapsule(0.6, 0.3, 0.1),
-    skeleton_files: 'girl.*',
+    skeleton_files: 'Girl.*',
     skeleton_toward: [0, 1],
-    body_file: 'body1.json',
+    body_file: 'Girl.body.json',
 });
 
-new Style('Style.Verify/1A', {
+new Style('Style.Verify^1A', {
     name: 'Style 1',
-    character: 'Character.Verify/1',
+    character: 'Character.Verify^1',
     attributes: {},
     slots: ['A2D2', 'A2D2', 'S1A2D2'],
     fixed_attributes,
-    perks: ['Perk.Verify/1A', 'Perk.Verify/1B'],
+    perks: ['Perk.Verify^1A', 'Perk.Verify^1B'],
     actions: [],
     view_model: 'StyleOne-1.vrm',
 });
 
-new Style('Style.Verify/1B', {
+new Style('Style.Verify^1B', {
     name: 'Style 1',
-    character: 'Character.Verify/1',
+    character: 'Character.Verify^1',
     attributes: {},
     slots: ['A1', 'A1', 'A1'],
     fixed_attributes,
     perks: [],
-    usable_perks: ['Perk.Verify/1A'],
+    usable_perks: ['Perk.Verify^1A'],
     actions: [],
     view_model: 'StyleOne-1.vrm',
 });
 
-new Character('Character.Verify/2', {
+new Character('Character.Verify^2', {
     name: 'Character 2',
     level: [1, 3],
-    styles: ['Style.Verify/2'],
-    equipments: ['Equipment.Verify/2A'],
+    styles: ['Style.Verify^2'],
+    equipments: ['Equipment.Verify^2A'],
     bounding: new TaperedCapsule(0.6, 0.3, 0.1),
     skeleton_files: 'skel.*',
     skeleton_toward: [0, 1],
     body_file: 'body2.json',
 });
 
-new Style('Style.Verify/2', {
+new Style('Style.Verify^2', {
     name: 'Style 2',
-    character: 'Character.Verify/2',
+    character: 'Character.Verify^2',
     attributes: {},
     slots: ['A1', 'A1', 'A1'],
     fixed_attributes,
-    perks: ['Perk.Verify/2A'],
+    perks: ['Perk.Verify^2A'],
     actions: [],
     view_model: 'StyleOne-1.vrm',
 });
 
-new Equipment('Equipment.Verify/1A', {
+new Equipment('Equipment.Verify^1A', {
     name: 'Weapon 1A',
-    character: 'Character.Verify/1',
+    character: 'Character.Verify^1',
     slot: Slot1,
     level: [1, 4],
     attributes: {},
     slots: ['', '', 'A1', 'S1A1'],
 });
 
-new Equipment('Equipment.Verify/1B', {
+new Equipment('Equipment.Verify^1B', {
     name: 'Weapon 1B',
-    character: 'Character.Verify/1',
+    character: 'Character.Verify^1',
     slot: Slot1,
     level: [1, 4],
     attributes: {},
 });
 
-new Equipment('Equipment.Verify/1C', {
+new Equipment('Equipment.Verify^1C', {
     name: 'Weapon 1B',
-    character: 'Character.Verify/1',
+    character: 'Character.Verify^1',
     slot: Slot3,
     level: [1, 2],
     attributes: {},
     slots: ['', 'D2'],
 });
 
-new Equipment('Equipment.Verify/2A', {
+new Equipment('Equipment.Verify^2A', {
     name: 'Weapon 2A',
-    character: 'Character.Verify/2',
+    character: 'Character.Verify^2',
     slot: Slot1,
     level: [1, 4],
     attributes: {},
@@ -102,25 +102,25 @@ new Equipment('Equipment.Verify/2A', {
     entries: {},
 });
 
-new Perk('Perk.Verify/1A', {
+new Perk('Perk.Verify^1A', {
     name: 'Verify 1A',
-    character: 'Character.Verify/1',
-    style: 'Style.Verify/1A',
+    character: 'Character.Verify^1',
+    style: 'Style.Verify^1A',
     max_level: 2,
-    usable_styles: ['Style.Verify/1B'],
+    usable_styles: ['Style.Verify^1B'],
     slots: ['', 'A1'],
 });
 
-new Perk('Perk.Verify/1B', {
+new Perk('Perk.Verify^1B', {
     name: 'Verify 1A',
-    character: 'Character.Verify/1',
-    style: 'Style.Verify/1A',
+    character: 'Character.Verify^1',
+    style: 'Style.Verify^1A',
     max_level: 2,
 });
 
-new Perk('Perk.Verify/2A', {
+new Perk('Perk.Verify^2A', {
     name: 'Verify 2A',
-    character: 'Character.Verify/2',
-    style: 'Style.Verify/2',
+    character: 'Character.Verify^2',
+    style: 'Style.Verify^2',
     max_level: 2,
 });
