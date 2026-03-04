8ba23d2f711a89b04c2ca3dcb6360878b2f97b45
SlimmeYummy <zzzcccnnn@outlook.com>
2026-03-04 20:38:33 +0800

    logic zone

--- critical-point/core/src/logic/test_utils.rs
+++ critical-point/core/src/logic/test_utils.rs
@@ -1,20 +1,12 @@
 use jolt_physics_rs::PhysicsSystem;
-use std::fmt::Debug;
-use std::rc::Rc;
 
 use crate::consts::TEST_ASSET_PATH;
-use crate::instance::{assemble_player, InstPlayer};
-use crate::logic::action::{
-    impl_state_action, ContextAction, LogicActionAny, LogicActionBase, LogicActionStatus, StateActionAny,
-    StateActionBase, StateActionType,
-};
+use crate::instance::InstCharacter;
 use crate::logic::game::{ContextUpdate, LogicSystems};
-use crate::logic::physics::{
-    BroadPhaseLayerInterfaceImpl, ObjectLayerPairFilterImpl, ObjectVsBroadPhaseLayerFilterImpl,
-};
+use crate::logic::physics::{PhyBroadPhaseLayerInterface, PhyObjectLayerPairFilter, PhyObjectVsBroadPhaseLayerFilter};
 use crate::parameter::ParamPlayer;
-use crate::template::{TmplDatabase, TmplType};
-use crate::utils::{extend, id, NumID, TmplID, XResult, MIN_PLAYER_ID};
+use crate::template::TmplDatabase;
+use crate::utils::{id, NumID, XResult};
 
 pub(super) struct TestEnv {
     pub systems: LogicSystems,
@@ -22,7 +14,6 @@ pub(super) struct TestEnv {
 
 impl TestEnv {
     pub const FRAME: u32 = 100;
-    pub const PLAYER_ID: NumID = MIN_PLAYER_ID + 1;
 
     pub fn new() -> XResult<TestEnv> {
         let db = TmplDatabase::new(10240, 150)?;
@@ -35,22 +26,22 @@ impl TestEnv {
     }
 }
 
-// pub(crate) fn mock_physics_system() -> PhysicsSystem {
-//     PhysicsSystem::new(
-//         BroadPhaseLayerInterfaceImpl::new_vbox(BroadPhaseLayerInterfaceImpl),
-//         ObjectVsBroadPhaseLayerFilterImpl::new_vbox(ObjectVsBroadPhaseLayerFilterImpl),
-//         ObjectLayerPairFilterImpl::new_vbox(ObjectLayerPairFilterImpl),
-//     )
-// }
+pub(crate) fn mock_physics_system() -> PhysicsSystem {
+    PhysicsSystem::new(
+        PhyBroadPhaseLayerInterface::new_vbox(PhyBroadPhaseLayerInterface),
+        PhyObjectVsBroadPhaseLayerFilter::new_vbox(PhyObjectVsBroadPhaseLayerFilter),
+        PhyObjectLayerPairFilter::new_vbox(PhyObjectLayerPairFilter),
+    )
+}
 
 // pub(crate) fn mock_logic_systems() -> LogicSystems {
 //     let db = TmplDatabase::new(10240, 150).unwrap();
 //     LogicSystems::new(db, TEST_ASSET_PATH, None).unwrap()
 // }
 
-// pub(crate) fn mock_logic_chara_physics(player_id: NumID, inst_player: Rc<InstPlayer>) {}
+// pub(crate) fn mock_logic_chara_physics(player_id: NumID, inst_player: Rc<InstCharacter>) {}
 
-// pub(crate) fn mock_inst_player(systems: &mut LogicSystems) -> Rc<InstPlayer> {
+// pub(crate) fn mock_inst_player(systems: &mut LogicSystems) -> Rc<InstCharacter> {
 //     let mut ctx = ContextUpdate::new(systems, 0, 0);
 //     let param_player = ParamPlayer {
 //         character: id!("Character.One"),
