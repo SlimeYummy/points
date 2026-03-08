ff127261ae52e4f8e190a26dd46c192e3a6442fb
SlimmeYummy <zzzcccnnn@outlook.com>
2026-03-08 22:01:19 +0800

    logic character physics

--- critical-point/core/src/logic/character/physics.rs
+++ critical-point/core/src/logic/character/physics.rs
@@ -8,20 +8,20 @@ use jolt_physics_rs::{
     self as jolt, vdata, Body, BodyCreationSettings, BodyID, BodyInterface, CharacterContactListener,
     CharacterContactListenerVTable, CharacterContactSettings, CharacterVirtual, CharacterVirtualSettings,
     ExtendedUpdateSettings, GroundState, JMut, JRef, JVec3, MotionType, MutableCompoundShape,
-    MutableCompoundShapeSettings, PhysicsMaterial, RotatedTranslatedShapeSettings, SubShapeID, SubShapeSettings,
-    TaperedCapsuleShapeSettings,
+    MutableCompoundShapeSettings, PhysicsMaterial, SubShapeID, SubShapeSettings,
 };
 use ozz_animation_rs::SKELETON_NO_PARENT;
 use std::cell::Cell;
 use std::rc::Rc;
 
-use super::LogicCharaAction;
-use crate::animation::rest_poses_to_model_transforms;
+use crate::animation::{rest_poses_to_model_transforms, HitSampler, HitTrackBase};
 use crate::consts::SPF;
-use crate::instance::InstPlayer;
+use crate::instance::InstCharacter;
+use crate::logic::character::LogicCharaAction;
 use crate::logic::game::{ContextRestore, ContextUpdate};
-use crate::logic::physics::PHY_LAYER_PLAYER;
-use crate::utils::{dir_xz_from_quat, quat_from_dir_xz, xerrf, xfrom, NumID, Symbol, XResult};
+use crate::logic::physics::phy_layer;
+use crate::logic::{LogicActionAnimationID, PhyBodyUserData};
+use crate::utils::{quat_from_dir_xz, xerrf, xfrom, NumID, Symbol, XResult};
 
 #[repr(C)]
 #[derive(
@@ -45,8 +45,8 @@ pub struct StateCharaPhysics {
 #[derive(Educe)]
 #[educe(Debug)]
 pub(crate) struct LogicCharaPhysics {
-    player_id: NumID,
-    inst_player: Rc<InstPlayer>,
+    chara_id: NumID,
+    inst_chara: Rc<InstCharacter>,
     velocity: Vec3A,
     position: Vec3A,
     direction: Vec2xz,
@@ -56,13 +56,17 @@ pub(crate) struct LogicCharaPhysics {
     #[educe(Debug(ignore))]
     character: JMut<CharacterVirtual<CharacterContactListenerImpl>>,
     target_body: BodyID,
+    #[educe(Debug(ignore))]
     target_shape: JMut<MutableCompoundShape>,
-    target_bindings: Vec<JointBinding>,
+    joint_bindings: Vec<JointBinding>,
+
+    hit_current_id: LogicActionAnimationID,
+    hit_bodies: Vec<BodyID>,
 
     cache_isometries: Vec<Isometry3A>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct JointBinding {
     position: Vec3A,
     rotation: Quat,
@@ -73,36 +77,41 @@ struct JointBinding {
 }
 
 impl LogicCharaPhysics {
-    pub fn new(
+    pub(crate) fn new(
         ctx: &mut ContextUpdate,
-        player_id: NumID,
-        inst_player: Rc<InstPlayer>,
+        chara_id: NumID,
+        inst_chara: Rc<InstCharacter>,
         position: Vec3A,
         direction: Vec2xz,
     ) -> XResult<LogicCharaPhysics> {
         let rotation = quat_from_dir_xz(direction);
-        let character = LogicCharaPhysics::init_bounding(ctx, &inst_player, position, rotation)?;
-        let (target_body, target_shape, target_bindings) =
-            LogicCharaPhysics::init_target(ctx, &inst_player, position, rotation)?;
+        let character = LogicCharaPhysics::init_bounding(ctx, &inst_chara, position, rotation)?;
+        let (target_body, target_shape, joint_bindings) =
+            LogicCharaPhysics::init_bodies(ctx, chara_id, &inst_chara, position, rotation)?;
 
-        let target_bindings_len = target_bindings.len();
+        let target_bindings_len = joint_bindings.len();
         Ok(LogicCharaPhysics {
-            player_id,
-            inst_player,
+            chara_id,
+            inst_chara,
             velocity: Vec3A::ZERO,
             position,
             direction,
             rotation,
             idle: Cell::new(true),
+
             character,
             target_body,
             target_shape,
-            target_bindings,
+            joint_bindings,
+
+            hit_current_id: LogicActionAnimationID::INVALID,
+            hit_bodies: Vec::with_capacity(16),
+
             cache_isometries: Vec::with_capacity(target_bindings_len),
         })
     }
 
-    pub fn state(&self) -> StateCharaPhysics {
+    pub(crate) fn state(&self) -> StateCharaPhysics {
         StateCharaPhysics {
             velocity: self.velocity,
             position: self.position,
@@ -110,7 +119,7 @@ impl LogicCharaPhysics {
         }
     }
 
-    pub fn restore(&mut self, _ctx: &ContextRestore, state: &StateCharaPhysics) -> XResult<()> {
+    pub(crate) fn restore(&mut self, _ctx: &ContextRestore, state: &StateCharaPhysics) -> XResult<()> {
         self.velocity = state.velocity;
         self.position = state.position;
         self.direction = state.direction;
@@ -120,27 +129,15 @@ impl LogicCharaPhysics {
 
     fn init_bounding(
         ctx: &mut ContextUpdate,
-        inst_player: &InstPlayer,
+        inst_chara: &InstCharacter,
         position: Vec3A,
         rotation: Quat,
     ) -> XResult<JMut<CharacterVirtual<CharacterContactListenerImpl>>> {
-        let bounding = inst_player.bounding;
-        let mut chara_shape = jolt::create_tapered_capsule_shape(&TaperedCapsuleShapeSettings::new(
-            bounding.half_height,
-            bounding.top_radius,
-            bounding.bottom_radius,
-        ))
-        .map_err(xfrom!())?;
-        chara_shape = jolt::create_rotated_translated_shape(&RotatedTranslatedShapeSettings::new(
-            chara_shape,
-            Vec3A::new(0.0, bounding.half_height + bounding.bottom_radius * 1.25, 0.0),
-            Quat::IDENTITY,
-        ))
-        .map_err(xfrom!())?;
+        let charc_phy = ctx.asset.load_character_physics(inst_chara.skeleton_files)?;
 
         let mut character = CharacterVirtual::new(
             &mut ctx.physics,
-            &CharacterVirtualSettings::new(chara_shape),
+            &CharacterVirtualSettings::new(charc_phy.bounding),
             position.into(),
             rotation,
         );
@@ -153,80 +150,84 @@ impl LogicCharaPhysics {
         Ok(character)
     }
 
-    fn init_target(
+    fn init_bodies(
         ctx: &mut ContextUpdate,
-        inst_player: &InstPlayer,
+        chara_id: NumID,
+        inst_chara: &InstCharacter,
         position: Vec3A,
         rotation: Quat,
     ) -> XResult<(BodyID, JMut<MutableCompoundShape>, Vec<JointBinding>)> {
-        let skeleton = ctx.asset.load_skeleton(inst_player.skeleton_files)?;
-        let mut loaded_target_box = ctx.asset.load_target_box(&inst_player.body_file)?;
+        let skeleton = ctx.asset.load_skeleton(inst_chara.skeleton_files)?;
+        let mut charc_phy = ctx.asset.load_character_physics(inst_chara.skeleton_files)?;
 
         let mut model_transforms = vec![Transform3A::ZERO; skeleton.num_joints()];
         rest_poses_to_model_transforms(&skeleton, &mut model_transforms)?;
-        let mut target_bindings = Vec::with_capacity(loaded_target_box.bindings.len());
-        let mut sub_shape_settings = Vec::with_capacity(loaded_target_box.bindings.len());
+        let mut joint_bindings = Vec::with_capacity(charc_phy.bodies.len());
+        let mut sub_shape_settings = Vec::with_capacity(charc_phy.bodies.len());
 
-        for loaded_binding in loaded_target_box.bindings.drain(..) {
-            let joint = skeleton.joint_by_name(loaded_binding.joint.as_str()).ok_or_else(
-                || xerrf!(BadAsset; "target_box={}, joint={}", &inst_player.skeleton_files, &loaded_binding.joint),
-            )?;
+        for body in charc_phy.bodies.drain(..) {
+            let joint = skeleton
+                .joint_by_name(body.joint.as_str())
+                .ok_or_else(|| xerrf!(BadAsset; "target_box={}, joint={}", &inst_chara.skeleton_files, &body.joint))?;
 
             let mut joint2 = SKELETON_NO_PARENT as i16;
-            if !loaded_binding.joint2.is_empty() {
-                joint2 = skeleton.joint_by_name(loaded_binding.joint2.as_str()).ok_or_else(
-                    || xerrf!(BadAsset; "target_box={}, joint2={}", &inst_player.skeleton_files, &loaded_binding.joint2),
+            if !body.joint2.is_empty() {
+                joint2 = skeleton.joint_by_name(body.joint2.as_str()).ok_or_else(
+                    || xerrf!(BadAsset; "target_box={}, joint2={}", &inst_chara.skeleton_files, &body.joint2),
                 )?
             };
 
-            target_bindings.push(JointBinding {
-                position: loaded_binding.position.into(),
-                rotation: loaded_binding.rotation,
-                part: loaded_binding.part,
+            joint_bindings.push(JointBinding {
+                position: body.position,
+                rotation: body.rotation,
+                part: body.part,
                 joint,
-                ratio: loaded_binding.ratio,
+                ratio: body.ratio,
                 joint2,
             });
 
             let transform = model_transforms[joint as usize];
-            let rotation = transform.rotation * loaded_binding.rotation;
+            let rotation = transform.rotation * body.rotation;
             let position = if joint2 < 0 {
-                transform.translation + transform.rotation * loaded_binding.position
+                transform.translation + transform.rotation * body.position
             }
             else {
                 Vec3A::lerp(
                     transform.translation,
                     model_transforms[joint2 as usize].translation,
-                    loaded_binding.ratio,
-                ) + transform.rotation * loaded_binding.position
+                    body.ratio,
+                ) + transform.rotation * body.position
             };
 
-            sub_shape_settings.push(SubShapeSettings::new(loaded_binding.shape, position, rotation));
+            sub_shape_settings.push(SubShapeSettings::new(body.shape, position, rotation));
         }
 
         let target_box_settings = MutableCompoundShapeSettings::new(&sub_shape_settings);
         let mut target_shape = jolt::create_mutable_compound_shape_mut(&target_box_settings).map_err(xfrom!())?;
+
+        let mut settings = BodyCreationSettings::new(
+            unsafe { target_shape.steal_ref().into() },
+            phy_layer!(Target, inst_chara.is_player => Player | Enemy),
+            MotionType::Kinematic,
+            position.into(),
+            rotation * inst_chara.skeleton_rotation,
+        );
+        settings.user_data = PhyBodyUserData::new_character(chara_id).into();
+
         let target_body = ctx
             .physics
             .body_itf()
-            .create_add_body(
-                &BodyCreationSettings::new(
-                    unsafe { target_shape.steal_ref().into() },
-                    PHY_LAYER_PLAYER,
-                    MotionType::Kinematic,
-                    position.into(),
-                    rotation * inst_player.skeleton_rotation,
-                ),
-                false,
-            )
+            .create_add_body(&settings, true)
             .map_err(xfrom!())?;
 
-        Ok((target_body, target_shape, target_bindings))
+        Ok((target_body, target_shape, joint_bindings))
     }
 
-    pub fn update(&mut self, ctx: &mut ContextUpdate, action: &LogicCharaAction) -> XResult<()> {
+    pub(crate) fn update(&mut self, ctx: &mut ContextUpdate, action: &LogicCharaAction) -> XResult<()> {
+        self.cache_isometries.clear();
         self.update_bounding(ctx, action)?;
-        self.update_target_box(ctx, action)?;
+        self.update_bodies(ctx, action)?;
+        self.update_hits(ctx, action)?;
         Ok(())
     }
 
@@ -269,11 +270,12 @@ impl LogicCharaPhysics {
         }
 
         self.character.extended_update(
-            PHY_LAYER_PLAYER,
+            phy_layer!(Bounding, self.inst_chara.is_player => Player | Enemy),
             SPF,
             gravity.into(),
             &ExtendedUpdateSettings::default(),
         );
+
         ctx.physics
             .body_itf()
             .set_position(self.target_body, self.character.get_position(), true);
@@ -289,11 +291,9 @@ impl LogicCharaPhysics {
         Ok(())
     }
 
-    fn update_target_box(&mut self, ctx: &mut ContextUpdate, action: &LogicCharaAction) -> XResult<()> {
-        self.cache_isometries.clear();
-
+    fn update_bodies(&mut self, ctx: &mut ContextUpdate, action: &LogicCharaAction) -> XResult<()> {
         let model_transforms = action.model_transforms();
-        for binding in &self.target_bindings {
+        for binding in &self.joint_bindings {
             let transform = model_transforms[binding.joint as usize];
             let rotation = transform.rotation * binding.rotation;
             let position = if binding.joint2 < 0 {
@@ -318,9 +318,94 @@ impl LogicCharaPhysics {
         ctx.physics.body_itf().set_position_rotation(
             self.target_body,
             self.position,
-            self.rotation * self.inst_player.skeleton_rotation,
+            self.rotation * self.inst_chara.skeleton_rotation,
             true,
         );
+        Ok(())
+    }
+
+    fn update_hits(&mut self, ctx: &mut ContextUpdate, action: &LogicCharaAction) -> XResult<()> {
+        let (current_id, sampler) = match action.hit_motion_info() {
+            Some(info) => (info.0, Some(info.1)),
+            None => (LogicActionAnimationID::INVALID, None),
+        };
+
+        let body_itf = ctx.physics.body_itf();
+        if current_id != self.hit_current_id {
+            for body_id in self.hit_bodies.drain(..) {
+                if body_id.is_valid() {
+                    body_itf.remove_body(body_id);
+                    body_itf.destroy_body(body_id);
+                }
+            }
+            self.hit_current_id = current_id;
+
+            if let Some(sampler) = sampler {
+                self.hit_bodies.resize(sampler.tracks_count(), BodyID::INVALID);
+            }
+        }
+
+        if let Some(sampler) = sampler {
+            let mut body_idx = 0;
+            for (track_idx, joint) in sampler.joints().iter().enumerate() {
+                let track = &sampler.hit_motion.joint_tracks[track_idx];
+                debug_assert_eq!(track.hit_id, joint.hit_id);
+                self.update_hit_body(ctx, body_idx, track, joint)?;
+                body_idx += 1;
+            }
+
+            for (track_idx, weapon) in sampler.weapons().iter().enumerate() {
+                let track = &sampler.hit_motion.weapon_tracks[track_idx];
+                debug_assert_eq!(track.hit_id, weapon.hit_id);
+                self.update_hit_body(ctx, body_idx, track, weapon)?;
+                body_idx += 1;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn update_hit_body<S>(
+        &mut self,
+        ctx: &mut ContextUpdate,
+        body_idx: usize,
+        track: &HitTrackBase,
+        sampler: &HitSampler<S>,
+    ) -> XResult<()> {
+        let body_itf = ctx.physics.body_itf();
+
+        if let Some(isometry) = sampler.isometry() {
+            let hit_isometry =
+                Isometry3A::new_3a(self.position, self.rotation * self.inst_chara.skeleton_rotation) * *isometry;
+
+            if self.hit_bodies[body_idx] == BodyID::INVALID {
+                let mut settings = BodyCreationSettings::new_sensor(
+                    track.shape.clone(),
+                    phy_layer!(Hit, self.inst_chara.is_player => Enemy | Player),
+                    MotionType::Static,
+                    hit_isometry.translation,
+                    hit_isometry.rotation,
+                );
+                settings.user_data = PhyBodyUserData::new_hit(self.chara_id, track.hit_id).into();
+                self.hit_bodies[body_idx] = body_itf.create_add_body(&settings, true).map_err(xfrom!())?;
+            }
+            else {
+                body_itf.set_position_rotation(
+                    self.hit_bodies[body_idx],
+                    hit_isometry.translation,
+                    hit_isometry.rotation,
+                    true,
+                );
+            }
+        }
+        else {
+            if self.hit_bodies[body_idx] != BodyID::INVALID {
+                body_itf.remove_body(self.hit_bodies[body_idx]);
+                body_itf.destroy_body(self.hit_bodies[body_idx]);
+                self.hit_bodies[body_idx] = BodyID::INVALID;
+            }
+        }
+
         Ok(())
     }
 }
