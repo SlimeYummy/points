1c9937b01678b1f7339633fd205d9700a0f30c07
SlimmeYummy <zzzcccnnn@outlook.com>
2026-04-06 20:04:49 +0800

    logic character value

--- critical-point/core/src/logic/character/mod.rs
+++ critical-point/core/src/logic/character/mod.rs
@@ -1,11 +1,16 @@
 mod action;
 mod ai;
 mod character;
+mod hit;
 mod physics;
-mod values;
+mod value;
 
 pub use action::StateCharaAction;
 pub(crate) use action::*;
 pub use character::*;
+pub(crate) use hit::*;
+pub use hit::{StateCharaHit, StateCharaHitBoxPair, StateCharaHitGroupPair};
 pub use physics::StateCharaPhysics;
 pub(crate) use physics::*;
+pub use value::StateCharaValue;
+pub(crate) use value::*;
