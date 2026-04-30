cf185b8ccf93d9097f5814364dd36b3a6f867c23
SlimmeYummy <zzzcccnnn@outlook.com>
2026-04-30 21:40:07 +0800

    logic character

--- critical-point/core/src/logic/character/mod.rs
+++ critical-point/core/src/logic/character/mod.rs
@@ -1,15 +1,13 @@
-mod action;
-mod ai;
 mod character;
-mod hit;
+mod control;
 mod physics;
 mod value;
 
-pub use action::StateCharaAction;
-pub(crate) use action::*;
 pub use character::*;
-pub(crate) use hit::*;
-pub use hit::{StateCharaHit, StateCharaHitBoxPair, StateCharaHitGroupPair};
+pub use control::StateCharaControl;
+pub(crate) use control::*;
+// pub(crate) use hit::*;
+// pub use hit::{StateCharaHit, StateCharaHitBoxPair, StateCharaHitGroupPair};
 pub use physics::StateCharaPhysics;
 pub(crate) use physics::*;
 pub use value::StateCharaValue;
