4ddc97f80cb310087919470902a04e7e37469c8c
SlimmeYummy <zzzcccnnn@outlook.com>
2026-05-16 23:38:45 +0800

    instance ai task idle

--- critical-point/core/src/instance/ai_task/idle.rs
+++ critical-point/core/src/instance/ai_task/idle.rs
@@ -1,14 +1,16 @@
 use crate::instance::ai_task::base::{InstAiTaskAny, InstAiTaskBase};
-use crate::template::{At, TmplAiTaskIdle};
-use crate::utils::{AiTaskType, TimeRange, TmplID, extend};
+use crate::template::{At, TmplAiTaskIdle, TmplAiTaskIdleStep};
+use crate::utils::{AiTaskType, TmplID, extend};
+
+pub type InstAiTaskIdleStep = TmplAiTaskIdleStep;
 
 #[repr(C)]
 #[derive(Debug)]
 pub struct InstAiTaskIdle {
     pub _base: InstAiTaskBase,
-    pub max_repeat: u32,
     pub action_idle: TmplID,
-    pub duration: TimeRange,
+    pub action_move: TmplID,
+    pub route: Vec<InstAiTaskIdleStep>,
 }
 
 extend!(InstAiTaskIdle, InstAiTaskBase);
@@ -29,15 +31,21 @@ impl InstAiTaskIdle {
     pub(crate) fn new(tmpl: At<TmplAiTaskIdle>) -> InstAiTaskIdle {
         InstAiTaskIdle {
             _base: InstAiTaskBase { tmpl_id: tmpl.id },
-            max_repeat: tmpl.max_repeat.to_native(),
             action_idle: tmpl.action_idle,
-            duration: tmpl.duration,
+            action_move: tmpl.action_move,
+            route: tmpl.route.iter().map(InstAiTaskIdleStep::from_rkyv).collect(),
         }
     }
 
     #[inline]
-    pub fn actions(&self) -> impl Iterator<Item = TmplID> {
-        std::iter::once(self.action_idle)
+    fn actions(&self) -> impl Iterator<Item = TmplID> + '_ {
+        std::iter::from_coroutine(
+            #[coroutine]
+            || {
+                yield self.action_idle;
+                yield self.action_move;
+            },
+        )
     }
 }
 
@@ -46,18 +54,22 @@ mod tests {
     use super::*;
     use crate::template::TmplDatabase;
     use crate::utils::id;
+    use glam::Vec3A;
 
     #[test]
     fn test_new_inst_ai_task_idle() {
         let db = TmplDatabase::new(10240, 150).unwrap();
 
-        let tmpl = db.find_as::<TmplAiTaskIdle>(id!("AiTask.NpcInstance.Idle^1")).unwrap();
+        let tmpl = db.find_as::<TmplAiTaskIdle>(id!("AiTask.InstanceNpc.Idle^1")).unwrap();
         let inst = InstAiTaskIdle::new(tmpl);
 
-        assert_eq!(inst.tmpl_id, id!("AiTask.NpcInstance.Idle^1"));
-        assert_eq!(inst.max_repeat, 1);
-        assert_eq!(inst.action_idle, id!("Action.NpcInstance.Idle^1A"));
-        assert_eq!(inst.duration.min(), 3.0);
-        assert_eq!(inst.duration.max(), 5.0);
+        assert_eq!(inst.tmpl_id, id!("AiTask.InstanceNpc.Idle^1"));
+        assert_eq!(inst.action_idle, id!("Action.InstanceNpc.Idle^1A"));
+        assert_eq!(inst.action_move, id!("Action.InstanceNpc.Walk^1A"));
+
+        assert_eq!(inst.route.len(), 3);
+        assert_eq!(inst.route[0], InstAiTaskIdleStep::Move(Vec3A::new(-3.0, 0.0, 0.0)));
+        assert_eq!(inst.route[1], InstAiTaskIdleStep::Idle(2.5));
+        assert_eq!(inst.route[2], InstAiTaskIdleStep::Move(Vec3A::new(0.0, 0.0, 3.0)));
     }
 }
